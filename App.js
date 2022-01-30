c7700f57f70bf28795aeb1ef091918806c8a11b2
Marcin Górski <m.gorski@macopedia.com>
2022-01-30 22:49:58 +0100

    init app

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+
+//Componrnts
+import Home from "./views/Home"
+import Login from "./views/Login"
+
+import RootStack from './navigators/RootStack'
+
+//styled components
+import { Container } from "./styles/appStyles"
+
+//async storage
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import AppLoading from 'expo-app-loading'
+import axios from 'axios'
+import consts from 'expo-constants';
+
+import { CredentialsContext } from './components/CredentialsContext'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ready, setReady] = useState(false);
+  const [isLogging, setIsLogging] = useState(true);
+  const [storedCredentials, setStoredCredentials] = useState(null);
+  const [todos, setTodos] = useState([]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  //mock todos
+  const mock = []
+
+  // const [todos, setTodos] = useState(mock)
+
+  const tryToLogin = () => {
+    AsyncStorage
+      .getItem("userCredentials")
+      .then(result => {
+        console.log(result)
+        if (result !== null) {
+          const todosUrl = `${consts.manifest.extra.apiUrl}/todos`
+          const parsedCredentials = JSON.parse(result)
+          console.log(parsedCredentials)
+          axios.get(todosUrl, {
+              headers: {
+                  Authorization: `Bearer ${parsedCredentials.accessToken}`
+              }
+          }).then(({ data }) => {
+              setTodos(data)
+              setIsLogging(false)
+              setStoredCredentials(parsedCredentials)
+          }).catch(err => {
+              setStoredCredentials(null)
+              setIsLogging(false)
+          })
+
+        } else {
+          setStoredCredentials(null)
+          setIsLogging(false)
+        }
+      })
+      .catch(err => console.log(err))
+  }
+
+
+  if (!ready || isLogging) {
+    return (
+      <AppLoading
+        startAsync={tryToLogin}
+        onFinish={() => { setReady(true) }}
+        onError={console.warn}
+      />
+    )
+  }
+
+  return (
+    <CredentialsContext.Provider value={{ storedCredentials, setStoredCredentials, todos, setTodos }}>
+      <RootStack />
+    </CredentialsContext.Provider>
+  )
+
+}
